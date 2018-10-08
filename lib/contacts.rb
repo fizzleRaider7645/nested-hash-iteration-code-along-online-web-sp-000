30b24eaafdd120ef21b27b8fdb78d43020435cd4
Douglas Uretsky <douglasuretsky@gmail.com>
2018-10-08 15:40:15 +0000

    automatically backed up by learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,7 +17,7 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |contact, values|
     if values[:favorite_ice_cream_flavors].include?("strawberry")
-      values[favorite_ice_cream_flavors].delete("strawberry")
+      values[:favorite_ice_cream_flavors].delete("strawberry")
     end
   end
   contacts
